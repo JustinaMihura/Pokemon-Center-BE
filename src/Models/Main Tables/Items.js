b9136d3faed3b22c85b08441f31d0d8072780631
JustinaMihura <justinamihura@hotmail.com>
2025-02-26 19:01:34 -0300

    desarrollo db

---
+++ src/Models/Main Tables/Items.js
@@ -0,0 +1,45 @@
+const {DataTypes} = require("sequelize");
+
+module.exports = (sequelize) =>  {
+
+    sequelize.define("Items" , {
+        id : {
+            type : DataTypes.INTEGER,
+            primaryKey : true
+        }, 
+        name : {
+            type : DataTypes.STRING(50),
+            allowNull : false
+        },
+        baby_trigger_for : {
+            type : DataTypes.BOOLEAN,
+            allowNull : true
+        },
+        cost : {
+            type : DataTypes.INTEGER
+        },
+        img : {
+            type : DataTypes.STRING(100)
+        },
+        fling_power : {
+            type : DataTypes.INTEGER,
+            allowNull : true 
+        }
+        //held-by-pokemon.id -> Tabla intermedia = 
+                                // pokemon.id + version-detail(tabla inermedia ) = 
+                                                // rarity + version.id
+        //machines.id
+
+        //effect-entries.id -> effect , effect-short
+        //category-item.id
+        //flavor-text ->  tabla inermedia  = 
+                            //verion-group.id + text 
+        //attributes.id (n:n)
+        //fling-effect.id (1 a 1) allowNull true
+
+    }, 
+        {
+            timestamps : false
+        }
+    )
+}
